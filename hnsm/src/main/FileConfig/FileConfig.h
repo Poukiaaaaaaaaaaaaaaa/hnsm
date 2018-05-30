369dbc9433ce2e120ffdca58514fb048ca1d2c77
4mayun <34216545+4mayun@users.noreply.github.com>
2018-05-31 01:09:53 +0200

    enlevé 3 espaces

--- hnsm/src/main/FileConfig/FileConfig.h
+++ hnsm/src/main/FileConfig/FileConfig.h
@@ -13,7 +13,7 @@ public:
 	FileConfig(std::string filePath);
 	std::string getStr(const std::string label);
 	std::string getPath(const std::string label);
-	int	        getVal(const std::string label);
+	int	    getVal(const std::string label);
 
 	explicit operator bool();
 
